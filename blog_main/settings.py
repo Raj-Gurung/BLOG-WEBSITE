59f0983c238cce88eb457580870701f7fb37c1f2
Raj Gurung <rajgrg8848@gmail.com>
2025-05-27 13:55:30 +0545

    Fix URL routing structure

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'blog_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RajGrz$default',
+        'USER': 'RajGrz',
+        'PASSWORD': 'Shikazu8848', 
+        'HOST': 'RajGrz.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -123,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blogs/static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 
